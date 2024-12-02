b8d4cd892a91a298dc121b937adf1be2b94cd3d9
chenhailiang15 <chenhailiang_15@163.com>
2024-12-02 20:10:53 +0800

    update sh

--- code_multi_gpu/start_MGN.sh
+++ code_multi_gpu/start_MGN.sh
@@ -5,7 +5,7 @@
 export MASTER_ADDR="10.0.0.214"
 export MASTER_PORT="12355"
 
-model_name="AlexNet"
+model_name="ResNet18"
 nnodes=2
 node_rank=0
 nprocs_per_node=2
@@ -27,7 +27,7 @@ python RunMultiGPUNode.py --model_name ${model_name} --node_rank ${node_rank} --
 --batch_size ${batch_size} --total_epochs ${total_epochs} --worker_num ${worker_num} --squad_data_size ${squad_data_size} \
 --sample_interval ${sample_interval} --environ_flage --record_flage
 
-model_name="VGG16"
+model_name="ResNet50"
 # batch_size=8
 # layer_num=10
 # layer_feature=10
@@ -36,30 +36,36 @@ python RunMultiGPUNode.py --model_name ${model_name} --node_rank ${node_rank} --
 --batch_size ${batch_size} --total_epochs ${total_epochs} --worker_num ${worker_num} --squad_data_size ${squad_data_size} \
 --sample_interval ${sample_interval} --environ_flage --record_flage
 
-# model_name="GraphSage"
+model_name="MobileNetv2"
 # batch_size=16
-# python RunMultiGPUNode.py --model_name ${model_name} --node_rank ${node_rank} --nnodes ${nnodes} \
-# --nprocs_per_node ${nprocs_per_node} --gpu_id_list ${gpu_id_list} --layer_num ${layer_num} --layer_feature ${layer_feature} \
-# --batch_size ${batch_size} --total_epochs ${total_epochs} --worker_num ${worker_num} --squad_data_size ${squad_data_size} \
-# --sample_interval ${sample_interval} --environ_flage --record_flage
+python RunMultiGPUNode.py --model_name ${model_name} --node_rank ${node_rank} --nnodes ${nnodes} \
+--nprocs_per_node ${nprocs_per_node} --gpu_id_list ${gpu_id_list} --layer_num ${layer_num} --layer_feature ${layer_feature} \
+--batch_size ${batch_size} --total_epochs ${total_epochs} --worker_num ${worker_num} --squad_data_size ${squad_data_size} \
+--sample_interval ${sample_interval} --environ_flage --record_flage
 
-# model_name="ResNet18"
-# python RunMultiGPUNode.py --model_name ${model_name} --node_rank ${node_rank} --nnodes ${nnodes} \
-# --nprocs_per_node ${nprocs_per_node} --gpu_id_list ${gpu_id_list} --layer_num ${layer_num} --layer_feature ${layer_feature} \
-# --batch_size ${batch_size} --total_epochs ${total_epochs} --worker_num ${worker_num} --squad_data_size ${squad_data_size} \
-# --sample_interval ${sample_interval} --environ_flage --record_flage
+model_name="GCN"
+layer_num=5000       
+layer_feature=100
+python RunMultiGPUNode.py --model_name ${model_name} --node_rank ${node_rank} --nnodes ${nnodes} \
+--nprocs_per_node ${nprocs_per_node} --gpu_id_list ${gpu_id_list} --layer_num ${layer_num} --layer_feature ${layer_feature} \
+--batch_size ${batch_size} --total_epochs ${total_epochs} --worker_num ${worker_num} --squad_data_size ${squad_data_size} \
+--sample_interval ${sample_interval} --environ_flage --record_flage
 
-# model_name="ResNet50"
-# python RunMultiGPUNode.py --model_name ${model_name} --node_rank ${node_rank} --nnodes ${nnodes} \
-# --nprocs_per_node ${nprocs_per_node} --gpu_id_list ${gpu_id_list} --layer_num ${layer_num} --layer_feature ${layer_feature} \
-# --batch_size ${batch_size} --total_epochs ${total_epochs} --worker_num ${worker_num} --squad_data_size ${squad_data_size} \
-# --sample_interval ${sample_interval} --environ_flage --record_flage
+model_name="Bert"
+layer_num=10       
+layer_feature=10
+batch_size=8
+python RunMultiGPUNode.py --model_name ${model_name} --node_rank ${node_rank} --nnodes ${nnodes} \
+--nprocs_per_node ${nprocs_per_node} --gpu_id_list ${gpu_id_list} --layer_num ${layer_num} --layer_feature ${layer_feature} \
+--batch_size ${batch_size} --total_epochs ${total_epochs} --worker_num ${worker_num} --squad_data_size ${squad_data_size} \
+--sample_interval ${sample_interval} --environ_flage --record_flage
 
-# model_name="MobileNetv2"
-# python RunMultiGPUNode.py --model_name ${model_name} --node_rank ${node_rank} --nnodes ${nnodes} \
-# --nprocs_per_node ${nprocs_per_node} --gpu_id_list ${gpu_id_list} --layer_num ${layer_num} --layer_feature ${layer_feature} \
-# --batch_size ${batch_size} --total_epochs ${total_epochs} --worker_num ${worker_num} --squad_data_size ${squad_data_size} \
-# --sample_interval ${sample_interval} --environ_flage --record_flage
+model_name="GraphSage"
+batch_size=16
+python RunMultiGPUNode.py --model_name ${model_name} --node_rank ${node_rank} --nnodes ${nnodes} \
+--nprocs_per_node ${nprocs_per_node} --gpu_id_list ${gpu_id_list} --layer_num ${layer_num} --layer_feature ${layer_feature} \
+--batch_size ${batch_size} --total_epochs ${total_epochs} --worker_num ${worker_num} --squad_data_size ${squad_data_size} \
+--sample_interval ${sample_interval} --environ_flage --record_flage
 
 # model_name="AlexNet" 
 # python RunMultiGPUNode.py --model_name ${model_name} --node_rank ${node_rank} --nnodes ${nnodes} \
