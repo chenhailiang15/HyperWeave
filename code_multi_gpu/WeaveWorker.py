f0ff2f6cb2640479e11df0a4d3ad753b2599c531
18681203974 <chenhailiang_15@163.com>
2024-12-13 17:37:45 +0800

    1213

--- code_multi_gpu/WeaveWorker.py
+++ code_multi_gpu/WeaveWorker.py
@@ -4,7 +4,7 @@ import threading
 import os
 from Job import Job
 import time
-
+import queue
 
 class WeaveWorker:
     def __init__(self,print_level):
@@ -12,22 +12,38 @@ class WeaveWorker:
         self.comunicator=CommunicateClient("10.26.128.115",print_level=print_level)
         self.comunicator.start_connect()
         self.comunicator.start_listening(self.message_receive)
-
+        self.sub_thread_queue=queue.Queue()
+        self.buffer=""
         
     def message_receive(self,message):
-        print("master receive:\n", message)
-        job=Job()
-        job.load_string(message)
-        job.set_start_time(time.time())
+        self.buffer+=message
+        buffer_list=self.buffer.split("--end")
+        if len(buffer_list)>1:
+            for i in range(len(buffer_list)-1):
+                job=Job()
+                job.load_string(buffer_list[i])
+                job.set_start_time(time.time())
+                
+                sub_thread=threading.Thread(target=self.run_command,args=(job, job.command,))
+                sub_thread.start()
+                self.sub_thread_queue.put(sub_thread)
+                
+            self.buffer=buffer_list[len(buffer_list)-1]
         
-        sub_thread=threading.Thread(target=self.run_command,args=(job, job.command,))
-        sub_thread.start()
         
         
         
     def run_command(self,job, message):
         print("worker receive message:\n",message)
         back=os.system(message)
+        if back==0:
+            job.succeed()
+        else:
+            job.failed()
+            
+        job.set_end_time(time.time())
+        self.comunicator.send(job.to_string()+"--end")
+            
         print("命令执行返回结果：",back)
 
 
