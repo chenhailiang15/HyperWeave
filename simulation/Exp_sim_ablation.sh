b6459fdd7089753351f90429b32a843312c5b734
chenhailiang <chenhailiang_15@163.com>
2025-04-14 01:20:22 +0800

    0414

---
+++ simulation/Exp_sim_ablation.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#!/bin/bash
+
+
+gpu_mem_percent=0.9
+node_kind="cluster"                           #"s4*3090, s8*3090, 4*3090, 3*2080ti, 4*2080"
+model_kind="all_model"                       #"cv_model, all_model"
+job_num=10000
+print_level=10
+couple_init_iter_percent=0.2
+overshared_factor=2
+validation="False"
+job_come_time_factor=1
+
+trace_id=0
+job_together_flage="False"
+
+
+system="Weave"
+strategy="SRSF"
+mps_flage="True"
+sync_flage="True"
+gpu_select_mode="random"
+
+python WeaveMaster.py --system ${system} --strategy ${strategy} \
+    --node_kind ${node_kind} --model_kind ${model_kind} --gpu_mem_percent ${gpu_mem_percent} --job_num ${job_num}\
+    --print_level ${print_level} --write_trace --write_sum  --overshared_factor ${overshared_factor} \
+    --job_together_flage ${job_together_flage} --validation ${validation} --trace_id ${trace_id} \
+    --couple_init_iter_percent ${couple_init_iter_percent} --mps_flage ${mps_flage} --job_come_time_factor ${job_come_time_factor}\
+    --sync_flage ${sync_flage} --gpu_select_mode ${gpu_select_mode}
+
+
+
+system="Weave"
+strategy="BN-SRSF"
+mps_flage="True"
+sync_flage="True"
+
+python WeaveMaster.py --system ${system} --strategy ${strategy} \
+    --node_kind ${node_kind} --model_kind ${model_kind} --gpu_mem_percent ${gpu_mem_percent} --job_num ${job_num}\
+    --print_level ${print_level} --write_trace --write_sum  --overshared_factor ${overshared_factor} \
+    --job_together_flage ${job_together_flage} --validation ${validation} --trace_id ${trace_id} \
+    --couple_init_iter_percent ${couple_init_iter_percent} --mps_flage ${mps_flage} --job_come_time_factor ${job_come_time_factor}\
+    --sync_flage ${sync_flage}
+
+system="Weave"
+strategy="BN-SRSF"
+mps_flage="True"
+sync_flage="False"
+
+python WeaveMaster.py --system ${system} --strategy ${strategy} \
+    --node_kind ${node_kind} --model_kind ${model_kind} --gpu_mem_percent ${gpu_mem_percent} --job_num ${job_num}\
+    --print_level ${print_level} --write_trace --write_sum  --overshared_factor ${overshared_factor} \
+    --job_together_flage ${job_together_flage} --validation ${validation} --trace_id ${trace_id} \
+    --couple_init_iter_percent ${couple_init_iter_percent} --mps_flage ${mps_flage}  --job_come_time_factor ${job_come_time_factor}\
+    --sync_flage ${sync_flage}
+
